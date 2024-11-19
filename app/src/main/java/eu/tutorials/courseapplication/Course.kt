aa5cfc3366f239812dab332ad8ab25569833b883
kubula316 <kubula316@o2.pl>
2024-11-19 14:25:45 +0100

    added sections and contentScreen

--- app/src/main/java/eu/tutorials/courseapplication/Course.kt
+++ app/src/main/java/eu/tutorials/courseapplication/Course.kt
@@ -5,60 +5,62 @@ import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CourseDto(val code : String,
-                  val name : String,
-                  val description : String,
-                  val author : String,
-                  val participantsLimit: Int,
-                  val participantsNumber: Int,
-                  val imageUrl: String
-    )
+data class CourseDto(
+    val code: String,
+    val name: String,
+    val description: String,
+    val author: String,
+    val participantsLimit: Int,
+    val participantsNumber: Int,
+    val imageUrl: String
+)
 
 @Serializable
-data class Course(val code : String,
-                  val status : Status,
-                  val name : String,
-                  val description : String,
-                  val author : String,
-                  val startDate : String,
-                  val endDate : String,
-                  val participantsLimit: Int,
-                  val participantsNumber: Int,
-                  val participants: List<CourseMember>,
-                  val category: Category,
-                  val tags : List<String>,
-                  val imageUrl: String,
+data class Course(
+    val code: String,
+    val status: Status,
+    val name: String,
+    val description: String,
+    val author: String,
+    val startDate: String,
+    val endDate: String,
+    val participantsLimit: Int,
+    val participantsNumber: Int,
+    val participants: List<CourseMember>,
+    val category: Category,
+    val tags: List<String>,
+    val imageUrl: String,
     val sections: List<Section>
 )
 
 @Serializable
 data class Section(
     val title: String,
-    val position : Int,
-    val lessons : List<Lecture>
+    val position: Int,
+    val lessons: List<Lecture>
 )
 
 @Serializable
 data class Lecture(
     val id: Long,
-    val title:String,
-    val videoUrl:String,
+    val title: String,
+    val videoUrl: String,
     val description: String,
     val materialsUrls: List<String>,
-    val position:Int
+    val position: Int
 )
 
 data class Student(
-    val id : Long,
+    val id: Long,
     val firstName: String,
     val lastName: String,
     val email: String,
     val status: Status,
     val enrolledCourses: List<String>,
-    val profileImageUrl : String,
-    )
+    val profileImageUrl: String,
+)
 
-data class CourseResponse(val categories : List<Course>)
+data class CourseResponse(val categories: List<Course>)
 
 @Parcelize
 enum class Status : Parcelable {
@@ -69,8 +71,8 @@ enum class Status : Parcelable {
 @Parcelize
 @Serializable
 data class CourseMember(
-    val email : String,
-    val dateOfSign : String
+    val email: String,
+    val dateOfSign: String
 ) : Parcelable
 
 @Parcelize
--- app/src/main/java/eu/tutorials/courseapplication/screens/CourseContentScreen.kt
+++ app/src/main/java/eu/tutorials/courseapplication/screens/CourseContentScreen.kt
@@ -1,18 +1,37 @@
 package eu.tutorials.courseapplication.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
@@ -20,22 +39,33 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.exoplayer.ExoPlayer
 import coil.compose.rememberAsyncImagePainter
+import eu.tutorials.courseapplication.Lecture
 import eu.tutorials.courseapplication.MainViewModel
+import eu.tutorials.courseapplication.Section
 import eu.tutorials.courseapplication.navigation.CourseDetailsScreen
+
 
 @Composable
 fun CourseContentScreen(
     viewState: MainViewModel.CoursesState,
     modifier: Modifier = Modifier
-){
-    Box(modifier = modifier){
-        when{
-            viewState.loading -> { CircularProgressIndicator(modifier.align((Alignment.Center)))
+) {
+    Box(modifier = modifier.background(
+        color = MaterialTheme.colorScheme.primary.copy(alpha = 0.05f)
+    )) {
+        when {
+            viewState.loading -> {
+                CircularProgressIndicator(modifier.align((Alignment.Center)))
             }
-            viewState.error != null -> { Text(text = "${viewState.error}")
+
+            viewState.error != null -> {
+                Text(text = "${viewState.error}")
                 println(viewState.error)
             }
+
             else -> {
                 ShowCourseContentScreen(viewState)
             }
@@ -47,76 +77,108 @@ fun CourseContentScreen(
 fun ShowCourseContentScreen(viewState: MainViewModel.CoursesState) {
     Column(
         modifier = Modifier
-            .padding(16.dp),
+            .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = rememberAsyncImagePainter(viewState.courseDetails.imageUrl),
-            contentDescription = "Category Image",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(16f / 9f)
-        )
-        Text(
-            text = viewState.courseDetails.name,
-            color = Color.White,
-            style = TextStyle(
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Left,
-                fontSize = 30.sp
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp)
-        )
-        Text(
-            text = viewState.courseDetails.description,
-            color = Color.LightGray,
-            style = TextStyle(
-                fontWeight = FontWeight.Light,
-                textAlign = TextAlign.Left,
-                fontSize = 18.sp
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 12.dp)
-        )
-        Text(
-            text = "Members: ${viewState.courseDetails.participantsNumber}/${viewState.courseDetails.participantsLimit}",
-            color = if (viewState.courseDetails.participantsNumber < viewState.courseDetails.participantsLimit){
-                Color.Green} else Color.Red,
-            style = TextStyle(
-                fontWeight = FontWeight.Light,
-                textAlign = TextAlign.Left,
-                fontSize = 26.sp
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 6.dp)
-        )
-        Text(
-            text = "Creator: ${viewState.courseDetails.author}",
-            color = Color.LightGray,
-            style = TextStyle(
-                fontWeight = FontWeight.Light,
-                textAlign = TextAlign.Left,
-                fontSize = 18.sp
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 6.dp)
-        )
-        Button(
-            onClick = {  },
-            enabled = !viewState.loading,
-            modifier = Modifier.fillMaxWidth().padding(top = 12.dp)
-        ) {
-            if (viewState.loading) {
-                CircularProgressIndicator(modifier = Modifier.size(24.dp), color = Color.White)
-            } else {
-                Text(text = "Sign Up!")
-            }
+        SectionShowScreen(sections = viewState.courseDetails.sections, onLectureClick = {})
+    }
+}
+
+@Composable
+fun SectionShowScreen(sections: List<Section>, onLectureClick: () -> Unit) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        items(sections) { section ->
+            SectionBlock(section, onLectureClick)
         }
     }
 }
+
+@Composable
+fun SectionBlock(section: Section, onLectureClick: () -> Unit) {
+    val isExpanded = remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { isExpanded.value = !isExpanded.value }
+    ) {
+            Text(
+                text = section.title,
+                color = Color.White,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Left,
+                    fontSize = 28.sp
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 4.dp, start = 16.dp)
+
+            )
+
+            if (isExpanded.value) {
+                section.lessons.forEach() { lesson ->
+                    LessonBlock(lesson = lesson, onLectureClick = onLectureClick)
+                }
+
+            }
+
+
+    }
+}
+@Composable
+fun LessonBlock(lesson: Lecture, onLectureClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {  },
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Text(text = lesson.position.toString(),
+            modifier = Modifier.padding(18.dp),
+            style = TextStyle(
+                fontSize = 26.sp
+            ),
+        )
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row{
+                Icon(
+                    imageVector = Icons.Default.CheckCircle,
+                    contentDescription = "Check Icon",
+                    tint = Color.Green
+                )
+                Text(
+                    text = lesson.title,
+                    color = Color.White,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Normal,
+                        textAlign = TextAlign.Left,
+                        fontSize = 22.sp
+                    ),
+                    maxLines = 1
+                )
+            }
+            Text(modifier = Modifier.padding(top = 4.dp),
+                text = lesson.description,
+                color = Color.White,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Left,
+                    fontSize = 16.sp
+                ),
+                maxLines = 1
+            )
+        }
+    }
+
+}
+
+
+
+
